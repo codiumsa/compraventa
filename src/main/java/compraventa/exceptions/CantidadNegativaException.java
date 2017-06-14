6d87f7bd28d9d147d9d4477cb4ec6a27c490b95f
Jorge Ramírez <jorgeramirez1990@gmail.com>
2017-06-13 22:57:13 -0400

    Se convierte a proyecto Java EE.

--- src/main/java/compraventa/exceptions/CantidadNegativaException.java
+++ src/main/java/compraventa/exceptions/CantidadNegativaException.java
@@ -2,6 +2,8 @@ package compraventa.exceptions;
 
 public class CantidadNegativaException extends Exception {
 	
+	private static final long serialVersionUID = 4437007293137899096L;
+
 	public CantidadNegativaException() {
 		super("La cantidad no puede ser negativa");
 	}
--- src/main/java/compraventa/exceptions/StockMinimoException.java
+++ src/main/java/compraventa/exceptions/StockMinimoException.java
@@ -2,6 +2,8 @@ package compraventa.exceptions;
 
 public class StockMinimoException extends Exception {
 	
+	private static final long serialVersionUID = 7723605959927057620L;
+
 	public StockMinimoException() {
 		super("Cantidad insuficiente");
 	}
